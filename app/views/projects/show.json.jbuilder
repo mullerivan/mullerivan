69f0537f41b1354936d92a15228102ae99992e3e
Ivan Müller <mulleivan@gmail.com>
2013-12-27 02:15:04 +1300

    first commit

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :name, :description, :start, :finish, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mullerivan::Application.config.secret_key_base = '7cc7cd8e20f24896bac87b98c03251fd62bda388a06ef5c80fe5966824eb4c8cebe9f91c0d5e1c8fc78ed3fcf35c0eccbb1af628daba8a9a6f3b85798fa2c6d3'
