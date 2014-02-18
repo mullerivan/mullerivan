6f3b0eb08f03c931ab41696011ae9d337ef5e37e
Ivan Muller <mullerivan@gmail.com>
2014-02-18 20:37:27 +1300

    going better

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106124630) do
-
-  create_table "projects", force: true do |t|
-    t.string   "name"
-    t.string   "position"
-    t.string   "client"
-    t.text     "description"
-    t.integer  "duration"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
